59aa9a2f77b27abe14ae1f2d03626b792eba43e8
Shaikh Sharjeel <neelwater.src@gmail.com>
2026-01-15 04:01:21 +0530

    Add slider and FAQ toggle functionality

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
-function send(){
- alert("Thank you! Our technician will contact you shortly.");
-}
+//==========================Section_2_Slider=========================================
+
+let products=document.getElementsByClassName("sliderItem");
+
+let count=-1;
+
+forwardSlide.addEventListener("click",()=>{
+    count++;
+    if(count==products.length-3){
+        forwardSlide.style.display="none";        
+    }
+    products[count].style.display="none";
+})
+
+backwardSlide.addEventListener("click",()=>{
+    forwardSlide.style.display="block";
+    products[count].style.display="block";
+    count--;
+});
+//========================================Icons________Questins___Answere====================
+iconOne.addEventListener("click",()=>{
+  if(ansOne.style.display == "block"){
+    ansOne.style.display="none";
+  }
+  else{
+    ansOne.style.display="block";
+  }
+})
+iconTwo.addEventListener("click",()=>{
+  if(ansTwo.style.display=="block"){
+    ansTwo.style.display="none";
+  }
+  else{
+    ansTwo.style.display="block";
+  }
+})
+iconThree.addEventListener("click",()=>{
+  if(ansThree.style.display == "block"){
+    ansThree.style.display="none";
+  }
+  else{
+    ansThree.style.display="block";
+  }
+})
+iconFour.addEventListener("click",()=>{
+  if(ansFour.style.display == "block"){
+    ansFour.style.display="none";
+  }
+  else{
+    ansFour.style.display="block";
+  }
+})
